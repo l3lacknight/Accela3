1d00dc48de64082e5cdfdfbf6c2fe58b948186f7
l3lacknight <35308970+l3lacknight@users.noreply.github.com>
2020-01-03 10:23:27 -0500

    commit by emse tools 2020-01-03 10:23:25

--- SCRIPTS/Event/ASA;MCD!~!RENEWAL!NA.js
+++ SCRIPTS/Event/ASA;MCD!~!RENEWAL!NA.js
@@ -1,26 +1,24 @@
-										logDebug("BEGINNING OF ASA 4 RENEWAL")
+                                        logDebug("BEGINNING OF ASA 4 RENEWAL")
 var authCapId = getParentLicenseCapID(capId);
-										logDebug("AFTER getParentLicenseCapID(capId)")
-										logDebug("authCapId:" + authCapId)
-										logDebug("capId:" + capId)
+                                        logDebug("AFTER getParentLicenseCapID(capId)")
+                                        logDebug("authCapId:" + authCapId)
+                                        logDebug("capId:" + capId)
 prepareAppForRenewal();
-
-										logDebug("BEFORE assessRenewalDecalFee()")
+                                        logDebug("BEFORE assessRenewalDecalFee()")
 assessRenewalDecalFee();
-										logDebug("AFTER assessRenewalDecalFee()")
-										logDebug("authCapId:" + authCapId)
-										logDebug("BEFORE authCapId if statement")
+                                        logDebug("AFTER assessRenewalDecalFee()")
+                                        logDebug("authCapId:" + authCapId)
+                                        logDebug("BEFORE authCapId if statement")
 if(authCapId)
-	{	
-										logDebug("INSIDE authCapId if statement")
-										logDebug("BEFORE assessRenewalLateFees(authCapId)")
-		assessRenewalLateFees(authCapId);
-	}
-										logDebug("BEFORE feeExists(RENEWAL) if statement")
+    {
+                                        logDebug("INSIDE authCapId if statement")
+                                        logDebug("BEFORE assessRenewalLateFees(authCapId)")
+        assessRenewalLateFees(authCapId);
+    }
+                                        logDebug("BEFORE feeExists(RENEWAL) if statement")
 if(feeExists("RENEWAL") == false)
-{	
-										logDebug("INSIDE feeExists(RENEWAL) if statement")
-	updateFee("RENEWAL","MCD_AUTH_RENEW","FINAL",1,"Y");
+{
+                                        logDebug("INSIDE feeExists(RENEWAL) if statement")
+    updateFee("RENEWAL","MCD_AUTH_RENEW","FINAL",1,"Y");
 }
-
-										logDebug("END OF ASA 4 RENEWAL")
+                                        logDebug("END OF ASA 4 RENEWAL")
